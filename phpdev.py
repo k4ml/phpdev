b526b6b40e14804a3ad359365b721fe528d3f3fc
Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2013-08-07 12:00:33 +0800

    steal web.py code for static serving ;)

--- phpdev.py
+++ phpdev.py
@@ -27,9 +27,11 @@ import httplib
 import subprocess
 import cStringIO
 import traceback
+import posixpath
 
 from urlparse import urlparse
 from wsgiref.simple_server import make_server
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -115,8 +117,90 @@ def application(environ, start_response):
     start_response(status, headers)
     return [message.fp.read()]
 
+class StaticApp(SimpleHTTPRequestHandler):
+    """WSGI application for serving static files.
+
+    Original code - https://raw.github.com/webpy/webpy/master/web/httpserver.py
+    """
+    def __init__(self, environ, start_response):
+        self.headers = []
+        self.environ = environ
+        self.start_response = start_response
+
+    def send_response(self, status, msg=""):
+        self.status = str(status) + " " + msg
+
+    def send_header(self, name, value):
+        self.headers.append((name, value))
+
+    def end_headers(self):
+        pass
+
+    def log_message(*a): pass
+
+    def __iter__(self):
+        environ = self.environ
+
+        self.path = environ.get('PATH_INFO', '')
+        self.client_address = environ.get('REMOTE_ADDR','-'), \
+                              environ.get('REMOTE_PORT','-')
+        self.command = environ.get('REQUEST_METHOD', '-')
+
+        from cStringIO import StringIO
+        self.wfile = StringIO() # for capturing error
+
+        try:
+            path = self.translate_path(self.path)
+            etag = '"%s"' % os.path.getmtime(path)
+            client_etag = environ.get('HTTP_IF_NONE_MATCH')
+            self.send_header('ETag', etag)
+            if etag == client_etag:
+                self.send_response(304, "Not Modified")
+                self.start_response(self.status, self.headers)
+                raise StopIteration
+        except OSError:
+            pass # Probably a 404
+
+        f = self.send_head()
+        self.start_response(self.status, self.headers)
+
+        if f:
+            block_size = 16 * 1024
+            while True:
+                buf = f.read(block_size)
+                if not buf:
+                    break
+                yield buf
+            f.close()
+        else:
+            value = self.wfile.getvalue()
+            yield value
+
+class StaticMiddleware:
+    """WSGI middleware for serving static files."""
+    def __init__(self, app, prefix='/static/'):
+        self.app = app
+        self.prefix = prefix
+        
+    def __call__(self, environ, start_response):
+        path = environ.get('PATH_INFO', '')
+        path = self.normpath(path)
+        file_path, path_info, query_string = parse_url(path)
+        extension = file_path.split('/')[-1][-3:]
+
+        if extension != 'php':
+            return StaticApp(environ, start_response)
+        else:
+            return self.app(environ, start_response)
+
+    def normpath(self, path):
+        path2 = posixpath.normpath(urllib.unquote(path))
+        if path.endswith("/"):
+            path2 += "/"
+        return path2
+
 if __name__ == '__main__':
-    server = make_server('0.0.0.0', 8080, application)
+    server = make_server('0.0.0.0', 8080, StaticMiddleware(application))
     print "Running at http://127.0.0.1:8080 ..."
     try:
         server.serve_forever()
